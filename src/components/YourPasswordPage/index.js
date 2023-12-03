da93c9c40d4dde0f2cde6ed2797e688d5223341d
AZaid <zarreen2003@gmail.com>
2023-12-03 11:23:24 +0000

    the password manager code is commited third

--- src/components/YourPasswordPage/index.js
+++ src/components/YourPasswordPage/index.js
@@ -2,32 +2,37 @@ import PasswordsList from '../PasswordsList'
 
 import './index.css'
 
-const YourPasswordPage = () => (
-  <div>
+const YourPasswordPage = props => {
+  const {passwordsList} = props
+  return (
     <div>
-      <h1>Your Passwords</h1>
+      <div>
+        <h1>Your Passwords</h1>
+        <div>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/password-manager-search-img.png"
+            alt="search"
+          />
+          <input type="search" />
+        </div>
+      </div>
+
+      {passwordsList.map(eachpassword => (
+        <PasswordsList key={eachpassword.id} passwordDetails={eachpassword} />
+      ))}
+
+      <div>
+        <input type="check-box" />
+        <h1>Show Passwords</h1>
+      </div>
       <div>
         <img
-          src="https://assets.ccbp.in/frontend/react-js/password-manager-search-img.png"
-          alt="search"
+          src="https://assets.ccbp.in/frontend/react-js/no-passwords-img.png "
+          alt="no passwords"
         />
-        <input />
       </div>
     </div>
-
-    <PasswordsList />
-
-    <div>
-      <input type="check-box" />
-      <h1>Show Passwords</h1>
-    </div>
-    <div>
-      <img
-        src="https://assets.ccbp.in/frontend/react-js/no-passwords-img.png "
-        alt="no passwords"
-      />
-    </div>
-  </div>
-)
+  )
+}
 
 export default YourPasswordPage
